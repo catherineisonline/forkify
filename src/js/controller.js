4a49ac89bb569bf54b8672ca9ed6852bf4f36da4
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-08-02 15:02:32 +0400

    removed extra consoles

--- src/js/controller.js
+++ src/js/controller.js
@@ -95,7 +95,7 @@ const controlAddRecipe = async function (newRecipe) {
 
     // Upload the new recipe data
     await model.uploadRecipe(newRecipe);
-    console.log(model.state.recipe);
+    // console.log(model.state.recipe);
 
     // Render recipe
     recipeView.render(model.state.recipe);
