f05fc1c2d287507bb505f061d71f4ab45c6d4bd3
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-08-02 14:51:31 +0400

    fixing fraction

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -3,7 +3,6 @@ import View from "./View.js";
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from "url:../../img/icons.svg"; // Parcel 2
 // import { Fraction } from "fractional";
-const Fraction = require("fractional").Fraction;
 
 class RecipeView extends View {
   _parentElement = document.querySelector(".recipe");
@@ -131,7 +130,9 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ""
+        ing.quantity ? 
+        // new Fraction
+        (ing.quantity).toString() : ""
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
